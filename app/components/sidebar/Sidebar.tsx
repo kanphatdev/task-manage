766543c43217426e71af06ccbe1d895d4cb5e284
kanphatratrazer <kanphatratrazer28@gmail.com>
2024-03-13 22:07:44 +0700

    add sidebar

---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import styled from 'styled-components'
+
+function Sidebar() {
+  return (
+    <SidebarStylled>
+      
+    </SidebarStylled>
+  )
+}
+const SidebarStylled = styled.nav``;
+export default Sidebar
